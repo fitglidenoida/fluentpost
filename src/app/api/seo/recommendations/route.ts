b9ecb6c332c08a684f552308005eee2c988105bf
TrailBlaze Wellness <info@fitglide.in>
2025-08-26 12:30:23 +0530

    updated routes

--- src/app/api/seo/recommendations/route.ts
+++ src/app/api/seo/recommendations/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '../auth/[...nextauth]/route'
+import { authOptions } from '../../auth/[...nextauth]/route'
 import { SEOService } from '@/lib/seoService'
 import { prisma } from '@/lib/db'
 import { z } from 'zod'
